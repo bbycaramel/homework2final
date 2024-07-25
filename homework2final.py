875bf3e3cc905c4498830914b42aa598e2c7b96e
Purple Caramel <124286350+bbycaramel@users.noreply.github.com>
2024-07-25 16:39:25 +0300

    Add files via upload

---
+++ homework2final.py
@@ -0,0 +1,6 @@
+number_of_homework = 12
+number_of_hours_spent = 1.5
+course_name = "Python"
+average_completion_time = number_of_hours_spent / number_of_homework
+print (course_name, 'всего задач:', number_of_homework, "затрачено часов:", number_of_hours_spent, "среднее время выполнения", average_completion_time)
+
